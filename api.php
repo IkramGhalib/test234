0385101689f592b94d68ee879237f17a07d1e0d3
IkramGhalib <ikramghalib@gmail.com>
2021-08-26 15:11:54 +0500

    Ikram change

--- api.php
+++ api.php
@@ -7,9 +7,9 @@ $offpeak=$_POST['offpeak'];
 $sql="INSERT INTO data_reading(meter,peak,offpeak) Values('$meter','$peak','$offpeak')";
 $result=$conn->query($sql);
 if($result){
-	echo json_decode(array());
+	// echo json_decode(array());
 }
 else{
-	echo json_decode(array());
+	// echo json_decode(array());
 }
 ?>
