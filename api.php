74343bb595835ec8c03c4225b20fa5080b74aa4c
developerhani <developerhani01@gmail.com>
2021-08-26 15:02:48 +0500

    Hanif change Api

--- api.php
+++ api.php
@@ -1,7 +1,7 @@
 <?php
 // $readingId=$_POST['readingId'];
 include('opendb.php');
-$meter=$_POST['meter'];
+$meter=$_POST['meter121212'];
 $peak=$_POST['peak'];
 $offpeak=$_POST['offpeak'];
 $sql="INSERT INTO data_reading(meter,peak,offpeak) Values('$meter','$peak','$offpeak')";
